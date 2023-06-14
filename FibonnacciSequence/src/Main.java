ea4ac03bc122f4618d6abb634b5b661d4707abfe
Tim <roppa070@gmail.com>
2023-06-14 14:38:29 -0400

    Double Loop sur le menu pour rester la ou on est lors dun input invalide

--- FibonnacciSequence/src/Main.java
+++ FibonnacciSequence/src/Main.java
@@ -16,7 +16,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int option = 0;
-        do {
+
+        while (option != 2) {
             // Affichage du menu
             System.out.println("\nMenu:");
             System.out.println("1. Exécutez Fibonacci.");
@@ -25,46 +26,51 @@ public class Main {
 
             try {
                 option = scanner.nextInt();
+
+                switch (option) {
+                    case 1:
+                        boolean invalidInput = false;  // Indicateur d'entrées invalides
+                        do {
+                            System.out.println("Entrez l'indice n-ième voulu de la suite de Fibonacci \n" +
+                                    "La suite est affichée de 0 à 92\n" +
+                                    "Le temps d'exécution est affiché de 0 à 7500");
+                            try {
+                                int n = scanner.nextInt();
+                                if (n < 0) {
+                                    System.out.println("Veuillez entrer un nombre entier positif.");
+                                    invalidInput = true;
+                                } else if (n > 7500) {
+                                    System.out.println("Veuillez entrer un nombre inférieur ou égal à 7500");
+                                    invalidInput = true;
+                                } else {
+                                    invalidInput = false;
+                                    // Création et exécution du runner Fibonacci
+                                    FibonacciRunner gameEngine = new FibonacciRunner();
+                                    gameEngine.run(n);
+                                }
+                            } catch (InputMismatchException ime) {
+                                System.out.println("Veuillez entrer un nombre entier valide.");
+                                scanner.next();
+                                invalidInput = true;
+                            }
+                        } while (invalidInput);  // Loop jusqu'a une entrée valide
+                        break;
+                    case 2:
+                        // Sortie du programme
+                        System.out.println("Sortie du programme.");
+                        break;
+                    default:
+                        // Gestion des options invalides
+                        System.out.println("Option invalide. Veuillez réessayer.");
+                }
             } catch (InputMismatchException ime) {
                 System.out.println("Veuillez entrer un nombre entier valide.");
                 scanner.next();
-                continue;
             }
-
-            switch (option) {
-                case 1:
-                    System.out.println("Entrez l'indice n-ième voulu de la suite de Fibonacci (0 - 7500): ");
-                    int n = 0;
-                    try {
-                        // Récupération de l'indice de la suite de Fibonacci
-                        n = scanner.nextInt();
-                        if (n < 0) {
-                            System.out.println("Veuillez entrer un nombre entier positif.");
-                            continue;
-                        } else if (n > 7500) {
-                            System.out.println("Veuillez entrer un nombre inférieur ou égal à 7500");
-                            continue;
-                        }
-                    } catch (InputMismatchException ime) {
-                        System.out.println("Veuillez entrer un nombre entier valide.");
-                        scanner.next();
-                        continue;
-                    }
-                    // Création et exécution du runner Fibonacci
-                    FibonacciRunner gameEngine = new FibonacciRunner();
-                    gameEngine.run(n);
-                    break;
-                case 2:
-                    // Sortie du programme
-                    System.out.println("Sortie du programme.");
-                    break;
-                default:
-                    // Gestion des options invalides
-                    System.out.println("Option invalide. Veuillez réessayer.");
-            }
-        } while (option != 2);
+        }
 
         // Fermeture du scanner après utilisation
         scanner.close();
     }
+
 }
